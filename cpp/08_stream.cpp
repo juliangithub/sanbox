166979c533d3eb485b851f632a12a5d49e1c9979
juliangithub <cqpingicloud@icloud.com>
2018-10-16 18:42:01 +0800

    [add stream demo code ]

--- cpp/08_stream.cpp
+++ cpp/08_stream.cpp
@@ -16,6 +16,20 @@ using namespace std;
 //ofstream & print(ofstream&) //function declare, stream reference.
 int main(int argc, char const *argv[])
 {
-	
+	int ival;
+	while(cin >> ival, !cin.eof())
+	{
+		if (cin.bad())
+		{
+			throw runtime_error("IO stream corrupted");
+
+		}
+		if (cin.fail())
+		{
+			cerr << "bad data , try again !";
+			cin.clear(istream::failbit);
+			continue;
+		}
+	}
 	return 0;
 }
