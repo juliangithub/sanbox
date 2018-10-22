492998d5adfe9c5d8ea78bf50378706284a68605
juliangithub <cqpingicloud@icloud.com>
2018-10-22 21:29:46 +0800

    [add container eg.]

--- cpp/03_vector.cpp
+++ cpp/03_vector.cpp
@@ -13,6 +13,43 @@ using std::bitset;
 
 int main()
 {
+	std::vector<char> vc_none;
+	cout << vc_none.size() << endl;
+	cout << vc_none.empty() <<endl;
+	std::vector<char> vc1(1, '1');
+	std::vector<char> vc2(vc1);
+	std::vector<char> vc22 = vc1;
+	std::vector<char> vc3{'3','C'};
+	std::vector<char> vc33 = {'#','C','3'};
+	std::vector<char> vc4(vc3);
+	std::vector<char> vc5(20,'-');
+	cout<< "*******char vector********" << endl;
+	cout << vc1[0] <<endl;
+	cout << vc2[0] <<endl;
+	cout << vc22[0] <<endl;
+
+	cout << vc3[1] <<endl;
+	cout << vc33[2] <<endl;
+	cout << vc4[1] <<endl;
+	cout << vc5[19] <<endl;
+	cout<< "============================" << endl << endl <<endl;
+
+	std::vector<int> vi1(10);
+	std::vector<int> vi2(10, 5);
+	std::vector<int> vi3(vi2);
+	std::vector<int> vi4{11,22,33};
+	cout<< "******int vector*********" << endl;
+	cout << vi1[2] <<endl;
+	cout << vi2[3] <<endl;
+	cout << vi3[4] <<endl;
+	cout << vi4[2] <<endl;
+	cout<< "============================" << endl << endl <<endl;
+	std::vector<string> v3;
+	std::vector<string> v4(v3);
+	std::vector<string> v7{"str7"};
+	cout << "v7 size: "<< v7.size() <<endl;
+	std::vector<string> v8(3,"str8");
+	
 	vector<int> v_num(10, 168);
 	vector<string> v_str(8,"kkkkk");
 	for(vector<string>::size_type idx=0; idx != v_str.size(); idx++)
@@ -35,12 +72,12 @@ int main()
 	}
 	string init_str = "101101110";
 	bitset<32> vStr32(init_str); 
-	/*
+	
 	for(int idx = 0; idx != 32 ;idx++)
 	{
 		cout << "Vstr[" << idx << "]:" << vStr32[idx] << endl;
 	}	
-	*/
+	
 	cout << "vStr: " << vStr32 << endl;
 	
 	int x[10];
--- cpp/09_container.cpp
+++ cpp/09_container.cpp
@@ -15,19 +15,50 @@
 #include <list>
 #include <deque>
 using namespace std;
+//g++ vector.cpp -o vector.d -std=c++11
 int main(int argc, char const *argv[])
 {
-	
+//顺序容器
+//vector 支持快速随机访问
+//list  支持快速插入、删除
+//deque  双端队列
+
+//顺序容器适配器
+//stack 		LIFO
+//queue 		FIFO
+//priority_queue	QOS queue
+
 	int ia[]={0,1,2,3,4,5,6,7,8};
 	string sa[6] = {"aa", "bb", "cc", "dd", "ee", "ff"};
-	vector<string> v_str(sa, sa+6);
-	list<int> v_ilist(ia+4, ia+6);
+	//vector
+	vector<string> v_str(sa, sa+4);
 	vector<int> v_int(ia, ia+8);
-	list<string> v_slist(sa+6, sa);
-
 	vector<int> v0 (5,6);
 	vector<int> v1(v0);
-	//vector<int> v2(v0+2, v0+4);
+	vector<int> v2(ia+1, ia+4);
+
+	cout << v2.size() << endl;
+	cout << v2[0] << endl;
+	cout << v2[1] << endl;
+
+	cout << v_str[3] << endl;
+
 	vector<int> v3(8);
+	
+	//list
+	list<int> v_ilist(ia+4, ia+6);
+
+	vector<string>::iterator mid = v_str.begin() + v_str.size() /2;
+	list<string> slist(v_str.begin(), mid);
+	deque<string> dlist(mid, v_str.end());
+
+	cout << slist.size() <<endl;
+	cout << dlist.size() <<endl;
+	list<string> v_slist(sa, sa+2);
+
+	deque<int> dint{ 11, 22, 33, 44};
+
+	list<int> cp_dint(dint.begin(), dint.end());
+
 	return 0;
 }
