474e2826f7bd44caa0fb39c82c0a49a86ad441e5
juliangithub <cqpingicloud@icloud.com>
2018-10-23 10:02:08 +0800

    [tiny code ]

--- cpp/09_container.cpp
+++ cpp/09_container.cpp
@@ -31,34 +31,39 @@ int main(int argc, char const *argv[])
 	int ia[]={0,1,2,3,4,5,6,7,8};
 	string sa[6] = {"aa", "bb", "cc", "dd", "ee", "ff"};
 	//vector
-	vector<string> v_str(sa, sa+4);
 	vector<int> v_int(ia, ia+8);
 	vector<int> v0 (5,6);
 	vector<int> v1(v0);
 	vector<int> v2(ia+1, ia+4);
-
-	cout << v2.size() << endl;
-	cout << v2[0] << endl;
-	cout << v2[1] << endl;
-
-	cout << v_str[3] << endl;
-
 	vector<int> v3(8);
 	
+	cout << v2.size() << endl;
+	cout << v2[0] << endl;
+	cout << v2[1] << endl;	
+	
 	//list
+	//list initcial directly.
 	list<int> v_ilist(ia+4, ia+6);
-
-	vector<string>::iterator mid = v_str.begin() + v_str.size() /2;
-	list<string> slist(v_str.begin(), mid);
-	deque<string> dlist(mid, v_str.end());
-
-	cout << slist.size() <<endl;
-	cout << dlist.size() <<endl;
 	list<string> v_slist(sa, sa+2);
 
-	deque<int> dint{ 11, 22, 33, 44};
+	
+	//const list size_type
+	const list<int>::size_type list_size=64;
+	list<string> l_size(list_size, "emmm");
 
+
+	//deque
+	//deque enum
+	deque<int> dint{ 11, 22, 33, 44};
+	cout << dlist.size() <<endl;
 	list<int> cp_dint(dint.begin(), dint.end());
+	
+
+	vector<string> v_str(sa, sa+4);
+	cout << v_str[3] << endl;
+	vector<string>::iterator mid = v_str.begin() + v_str.size() /2;
+	list<string> l_head(v_str.begin(), mid);
+	deque<string> d_bottom(mid, v_str.end());
 
 	return 0;
 }
