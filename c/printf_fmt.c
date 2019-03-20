85f022bf7dc76b530e6a8a87407e9e519da989f8
juliangithub <cqpingicloud@icloud.com>
2019-03-20 19:32:07 +0800

    add new code demo

--- c/printf_fmt.c
+++ c/printf_fmt.c
@@ -11,14 +11,64 @@
 ================================================================*/
 //#include "printf_fmt.h"
 #include <stdio.h>
+
+enum{
+	SK_LOG_NULL = 0,
+	SK_LOG_ERROR,
+	SK_LOG_INFO,
+	SK_LOG_DEBUG,
+	SK_LOG_TRACE
+};
+
+#define SK_LOG_LEVEL SK_LOG_TRACE
+
+#define SK_ERROR(msg...)  \
+    if(SK_LOG_LEVEL >= SK_LOG_ERROR){ \
+        printf("\033[1;31m[SK_ERROR:%s %d]",__FUNCTION__,__LINE__);\
+        printf (msg); \
+        printf("\033[0m\n");\
+    }
+
+#define SK_INFO(msg...)  \
+    if(SK_LOG_LEVEL >= SK_LOG_INFO){\
+        printf("\033[1;36m[SK_INFO]");\
+        printf (msg); \
+        printf("\033[0m\n");\
+    }
+
+#define SK_DEBUG(msg...) \
+    if(SK_LOG_LEVEL >= SK_LOG_DEBUG){ \
+        printf("\033[1;33m[SK_DEBUG:%s %d]",__FUNCTION__,__LINE__);\
+        printf (msg); \
+        printf("\033[0m\n");\
+    }
+
+#define SK_TRACE() \
+    if(SK_LOG_LEVEL >= SK_LOG_TRACE){ \
+        printf("\033[1;35m[SK_TRACE:%s %d]\033[0m\n",__FUNCTION__,__LINE__);\
+    }
+
 int main(int argc, char const *argv[])
 {
-	int num = 10241024;
 
+	SK_TRACE();
+	SK_DEBUG("11111111111111111111");
+	SK_INFO("11111111111111111111");
+	SK_ERROR("11111111111111111111");
+
+
+	int num = 10241024;
+	unsigned int num_unit = 10241024;
+	printf("%u\n", num_unit);
 	printf("%#4x\n", num);
 	printf("%#4.4x\n", num);
 	printf("%#2.2x\n", num);
 	//printf("%#d\n", num);
 	printf("%#o\n", num);
+
+
+
+
+
 	return 0;
 }
--- c/sizeof.c
+++ c/sizeof.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
-int sizeof_point(int *num, char *chr, char*str){
-        printf("%d %d %d \n", sizeof(*num), sizeof(*chr), sizeof(*str));
+void sizeof_point(int *num, char *chr, char*str){
+        printf("%zu %zu %zu \n", sizeof(*num), sizeof(*chr), sizeof(*str));
+        printf("%zu %zu %zu \n", sizeof(num), sizeof(chr), sizeof(str));
+        void *void_p_int = num;
+        void *void_p_char = chr;
+        void *void_p_str = str;
+        printf("%zu %zu %zu \n", sizeof(void_p_int), sizeof(void_p_char), sizeof(void_p_str));
+        printf("%zu %zu %zu \n", sizeof(*void_p_int), sizeof(*void_p_char), sizeof(*void_p_str));
+        printf("%zu\n", sizeof(void *));
 }
 int main()
 {
         int num;
         char chr;
         char str[32]={0x0};
-
+		printf("%zu %zu %zu \n", sizeof(num), sizeof(chr), sizeof(str));
         sizeof_point(&num,&chr, str);
+
+        
+
+       return 0;
 
 }
